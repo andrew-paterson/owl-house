45a4aef819fb7fbdaa720fc64a20055f81c7d371
Andrew Paterson <andrew@hyraxbio.co.za>
2017-10-27 08:32:22 +0200

    All images display on the main pages for the artefacts.

--- app/components/route-specific/artefacts/artefacts-container.js
+++ app/components/route-specific/artefacts/artefacts-container.js
@@ -3,12 +3,26 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   contentMap: Ember.inject.service(),
 
-  title: function() {
+  artefactContentMap: function() {
     var currentArtefact = this.get('contentMap.artefacts').findBy('metaTitle', this.get('artefact'))
-    return currentArtefact.title;
+    return currentArtefact;
   }.property('artefact'),
+
+  title: function() {
+    return this.get('artefactContentMap.title');
+  }.property('artefactContentMap'),
 
   artefactContent: function() {
     return 'route-specific/artefacts/' + this.get('artefact');
   }.property('artefact'),
+
+  artefactImagePaths: function() {
+    var self = this;
+    var imageSuffixes = this.get('artefactContentMap.contentImageSuffixes');
+    var imagePaths = imageSuffixes.map(function(item, index, enumerable) {
+      var imageDirectoryPath = item.indexOf('c_') !== -1 ? '/images/contextualised-artefacts/' : '/images/studio/';
+      return imageDirectoryPath + self.get('artefact') + "_" + item + '.jpg';
+    });
+   return imagePaths;
+  }.property('artefactContentMap'),
 });
--- app/services/content-map.js
+++ app/services/content-map.js
@@ -7,118 +7,137 @@ export default Ember.Service.extend({
       metaTitle:'badge',
       title: 'Badge',
       materials: null,
-      dimensions: null
+      dimensions: 'L: 115; W: 86; D: 3',
+      contentImageSuffixes: ['s_1', 'c_1', 'c_2']
     }, {
       id: 1,
       metaTitle: 'belt',
       title: 'Belt',
       materials: 'Brass or painted tin',
-      dimensions: null
+      dimensions: 'L: 733; W:41; D: 7',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1', 'c_2']
     }, {
       id: 2,
       metaTitle: 'bottle',
       title: 'Bottle',
-      materials: 'blue glass, possibly medicinal',
-      dimensions: null
+      materials: 'Blue glass, possibly medicinal',
+      dimensions: 'L: 174; W: 79; D: 44',
+      contentImageSuffixes: ['s_1', 'c_1']
     }, {
       id: 3,
       metaTitle: 'deer',
       title: 'Deer Figurine',
       materials: 'Knysna blackwood or rosewood',
-      dimensions: null
+      dimensions: 'L: 290; W: 154; D: 60',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1', 'c_2', 'c_3']
     }, {
       id: 4,
       metaTitle: 'hide',
       title: 'Hide',
       materials: 'Springbok hide',
-      dimensions: null
+      dimensions: 'L: 730; W: 480; D: 3',
+      contentImageSuffixes: ['s_1', 'c_1', 'c_2']
       //Not done on home
     }, {
      id: 5,
-      metaTite:'hostess',
+      metaTitle:'hostess',
       title: 'Hostess (or meisie)',
-      materials: 'cement and glass',
-      dimensions: null
+      materials: 'Cement and glass',
+      dimensions: 'L: 290; W: 345; D: 170',
+      contentImageSuffixes: ['s_1', 'c_1', 'c_2']
     }, {
       id: 6,
       metaTitle: 'iconography',
       title: 'Iconography',
-      materials: 'framed postcard',
-      dimensions: null
+      materials: 'Framed postcard',
+      dimensions: 'L: 155; W: 106; D: 8',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1', 'c_2']
     }, {
       id: 7,
       metaTitle: 'jug',
       title: 'Jug',
-      materials: 'painted enamel',
-      dimensions: null
+      materials: 'Painted enamel',
+      dimensions: 'L: 321; W: 255; D: 165',
+      contentImageSuffixes: ['s_1', 'c_1']
     }, {
       id: 8,
       metaTitle: 'mermaid',
       title: 'Mermaid',
       materials: null,
-      dimensions: null
+      dimensions: 'L: 74; W: 59; D: 61',
+      contentImageSuffixes: ['s_1', 'c_1']
     }, {
       id: 9,
       metaTitle: 'mug',
       title: 'Mug',
-      materials: 'hand-painted porcelain',
-      dimensions: null
+      materials: 'Hand-painted porcelain',
+      dimensions: 'L: 81; W: 86; D; 70',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1', 'c_2']
     }, {
      id: 10,
-      metaTite:'nude',
+      metaTitle:'nude',
       title: 'Nude',
       materials: null,
-      dimensions: null
+      dimensions: 'L: 291; W: 213; D: 11',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1']
       //Not done on home.
     }, {
       id: 11,
       metaTitle: 'oil-lamp',
       title: 'Oil Lamp',
       materials: null,
-      dimensions: null
+      dimensions: 'L: 218; W: 123; D: 88',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1', 'c_2']
     }, {
       id: 12,
       metaTitle: 'red-lamp',
       title: 'Red Lamp',
       materials: null,
-      dimensions: null
+      dimensions: 'L: 51; W: 30; D: 30',
+      contentImageSuffixes: ['s_1', 'c_1', 'c_2']
     }, {
       id: 14,
       metaTitle: 'shell',
       title: 'Shell',
       materials: null,
-      dimensions: null
+      dimensions: 'L: 55; W: 31; D: 19',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1']
     }, {
      id: 15,
-      metaTite:'shirt',
+      metaTitle:'shirt',
       title: 'Shirt',
       materials: 'Lightweight cotton with silk cuff and metal clasps. Tapestry style peasant shirt with embroidery detail. Size small.',
-      dimensions: null
+      dimensions: 'L: 600; W: 560; D: 1',
+      contentImageSuffixes: ['s_1', 'c_1']
       //Not done on home
     }, {
       id: 16,
       metaTitle: 'shoes',
       title: 'Shoes',
       materials: null,
-      dimensions: null
+      dimensions: 'L: 225; H: 119: W: 68',
+      contentImageSuffixes: ['s_1', 's_2', 's_3', 's_4', 's_5', 'c_1']
     }, {
       id: 17,
       metaTitle: 'tin',
       title: 'Tin',
       materials: 'Painted tin containing the remnants of a sewing kit',
-      dimensions: null
+      dimensions: 'H: 74; L: 185; D: 185',
+      contentImageSuffixes: ['s_1', 's_2', 'c_1', 'c_2']
     }, {
       id: 18,
       metaTitle: 'windchime',
       title: 'Windchime',
-      materials: 'stainless silver',
-      dimensions: null
+      materials: 'Stainless silver',
+      dimensions: 'L: 73; W: 30; D: 30',
+      contentImageSuffixes: ['s_1', 'c_1', 'c_2', 'c_3']
     }, {
       id: 19,
       metaTitle: 'windmill',
       title: 'Windmill',
-      materials: 'paper, wood',
-      dimensions: null, thumbnail: 'windmill_c_1.jpg'
+      materials: 'Paper, wood',
+      dimensions: 'L: 115; W: 255; D: 165',
+      contentImageSuffixes: ['s_1', 's_2', 's_3', 'c_1', 'c_2']
     }
     ],
 });
